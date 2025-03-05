09a461d32c150b6ddc563b002c07f1cbf06f9d49
phatcharaphon <phatcharaphon.pp@mail.kmutt.ac.th>
2025-03-05 23:10:11 +0700

    first commit

---
+++ src/composable/getJudProjects.js
@@ -0,0 +1,106 @@
+const getAllProjects = async () => {
+  try {
+    const token = localStorage.getItem('token'); // ดึง token จาก localStorage
+    if (!token) throw new Error('User not authenticated');
+
+    const res = await fetch(import.meta.env.VITE_ROOT_API + '/api/projects', {
+      method: 'GET',
+      headers: {
+        "Authorization": token, // ใช้ token ที่ดึงมาได้
+        "Content-Type": "application/json"
+      },
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    } else {
+      throw new Error('Error, cannot get data!');
+    }
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+  const getProjectById = async (id) => {
+    try {
+      const res = await fetch(import.meta.env.VITE_ROOT_API + "/api/project/" + id, {
+        method: 'GET',
+        headers: {}
+      });
+  
+      if (res.ok) {
+        const data = await res.json();
+        console.log('Project Data:', data);  // ตรวจสอบข้อมูลที่ดึงมาจาก API
+        return data;
+      } else {
+        throw new Error('Error, cannot get data!');
+      }
+    } catch (error) {
+      console.log('Fetch error:', error);
+      return null;
+    }
+  };
+  const getSprintById = async (id) => {
+    try {
+      const res = await fetch(import.meta.env.VITE_ROOT_API + "/api/sprint/" + id, {
+        method: 'GET',
+        headers: {}
+      });
+  
+      if (res.ok) {
+        const data = await res.json();
+        console.log('Sprint Data:', data);  // ตรวจสอบข้อมูลที่ดึงมาจาก API
+        return data;
+      } else {
+        throw new Error('Error, cannot get data!');
+      }
+    } catch (error) {
+      console.log('Fetch error:', error);
+      return null;
+    }
+  };
+  const getUserById = async (id) => {
+    try {
+      const res = await fetch(import.meta.env.VITE_ROOT_API + "/api/profile" , {
+        method: 'GET',
+        headers: {
+           'Authorization': id
+        }
+      });
+  
+      if (res.ok) {
+        const data = await res.json();
+        console.log('User Data:', data);  // ตรวจสอบข้อมูลที่ดึงมาจาก API
+        return data;
+      } else {
+        throw new Error('Error, cannot get data!');
+      }
+    } catch (error) {
+      console.log('Fetch error:', error);
+      return null;
+    }
+  };
+  const getAllUsers = async (id) => {
+    try {
+      const res = await fetch(import.meta.env.VITE_ROOT_API + "/api/admin/users" , {
+        method: 'GET',
+        headers: {
+           "Authorization": "112357"
+        }
+      });
+  
+      if (res.ok) {
+        const data = await res.json();
+        console.log('User Data:', data);  // ตรวจสอบข้อมูลที่ดึงมาจาก API
+        return data;
+      } else {
+        throw new Error('Error, cannot get data!');
+      }
+    } catch (error) {
+      console.log('Fetch error:', error);
+      return null;
+    }
+  };
+  
+  export { getAllProjects,getProjectById,getSprintById,getUserById,getAllUsers };
---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import DashboardListProjects from '../views/DashboardListProjects.vue'
+// import ListProjects from '@/views/ListProjects.vue'
+// import TaskBoardVue from '@/views/TaskBoard.vue'
+// import CreateProjectVue from '@/views/CreateProject.vue'
+// import LoginVue from '@/views/Login.vue'
+// import SignUpVue from '@/views/SignUp.vue'
+// import ProfileVue from  '@/views/Profile.vue'
+// import ChangePasswordVue from '@/views/ChangePassword.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: DashboardListProjects
+    }
+    // ,
+    // {
+    //   path: '/projects',
+    //   name: 'Listprojects',
+    //   component: ListProjects
+    // },
+    // {
+    //   path: '/create',
+    //   name: 'CreateProject',
+    //   component: CreateProjectVue
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'Login',
+    //   component: LoginVue
+    // },
+    // {
+    //   path: '/signup',
+    //   name: 'SignUp',
+    //   component: SignUpVue
+    // },
+    // {
+    //   path: '/profile',
+    //   name: 'Profile',
+    //   component: ProfileVue
+    // },
+    // {
+    //   path: '/password',
+    //   name: 'Password',
+    //   component: ChangePasswordVue
+    // },
+    // {
+    //   path: '/sprint/:id',
+    //   name: 'Taskboard',
+    //   component: TaskBoardVue,
+    // }
+  ]
+})
+
+
+
+export default router
